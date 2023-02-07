afc2e7a494e28fbc6aab04fec51ec4815755bbd7
armaan <adhanji8@gmail.com>
2023-02-07 12:26:40 -0800

    passport starter code

---
+++ middleware/checkAuth.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+/*
+FIX ME (types) 😭
+*/
+export const ensureAuthenticated = (req: Request, res: Response, next: any) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/auth/login");
+}
+
+/*
+FIX ME (types) 😭
+*/
+export const forwardAuthenticated = (req: any, res: any, next: any) => {
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/dashboard");
+}
